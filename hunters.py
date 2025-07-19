b260b99b0923465f83f0879267b64c3b573a43db
Ben Diamond <benarthur.diamond@gmail.com>
2025-07-19 03:01:07 -0400

    Uses game_data and creature_data now

--- hunters.py
+++ hunters.py
@@ -1,20 +1,23 @@
+from __future__ import annotations
 import numpy as np
 import pygame
 import sys
 from typing import TypedDict, List, Tuple
 
-class CreatureData(TypedDict):
+
+class CreatureData(TypedDict, total=False):
     pos_x: float
     pos_y: float
     speed: float
     detect_range: float
 
+
 class Creature:
     def __init__(self, creature_data: CreatureData):
         cd = creature_data
-        self.pos = np.array([cd.get("pos_x", 0.0), cd.get("pos_y", 0.0)])
-        self.speed = cd.get("speed", 1.0)
-        self.detect_range = cd.get("detect_range", 10.0)
+        self.pos = np.array([cd.get("pos_x", 0), cd.get("pos_y", 0)], dtype=float)
+        self.speed = cd.get("speed", 1)
+        self.detect_range = cd.get("detect_range", 10)
 
     def can_detect(self, other):
         # magnitude of the difference vector = distance
@@ -28,14 +31,20 @@ class Creature:
 
 
 class Hunter(Creature):
-    def step(self, creatures):
-        for c in creatures:
+    def __init__(self, hunter_data: CreatureData):
+        self.is_stealth = False
+        super().__init__(hunter_data)
+
+    def step(self, game_data: HuntersGame):
+        for c in game_data.creatures:
             if isinstance(c, Prey) and self.can_detect(c):
                 self.chase(c)
-    
+                pass
+
     def chase(self, prey):
         vt = self.vector_to(prey)
         self.pos += vt
+        pass
 
 
 class Prey(Creature):
@@ -43,8 +52,8 @@ class Prey(Creature):
         self.is_alert = False
         super().__init__(prey_data)
 
-    def step(self, creatures):
-        for c in creatures:
+    def step(self, game_data: HuntersGame):
+        for c in game_data.creatures:
             if isinstance(c, Hunter) and self.can_detect(c):
                 self.make_alert()
                 self.flee(c)
@@ -66,18 +75,21 @@ class Prey(Creature):
             self.is_alert = False
             self.detect_range *= 0.5
 
+
 class GameData(TypedDict):
     creatures: List[Creature]
     map_dim: Tuple[float, float]
 
-class HuntersGame:
-    # game_data keys
-    CREATURES = 0
-    MAP_DIM = 1
 
-    def __ini__(self, game_data):
+class HuntersGame:
+    def __init__(self, game_data):
         self.creatures = game_data["creatures"]
         self.map_dim = game_data["map_dim"]
+
+    def step(self):
+        for c in self.creatures:
+            c.step(self)  # pass game data
+
 
 def draw_creature(c, screen):
     color = [0, 0, 0]
@@ -88,30 +100,37 @@ def draw_creature(c, screen):
 
     pygame.draw.circle(screen, color, (c.pos[0], c.pos[1]), 5)
 
+
+def init_game_data():
+    hunter_data: CreatureData = {
+        "pos_x": 10,
+        "pos_y": 10,
+        # "speed": 1,
+        "detect_range": 500,
+    }
+
+    prey_data: CreatureData = {
+        "pos_x": 100,
+        "pos_y": 150,
+        "speed": 3,
+        "detect_range": 100,
+    }
+
+    game_data: GameData = {
+        "creatures": [Hunter(hunter_data), Prey(prey_data)],
+        "map_dim": (500, 500),
+    }
+
+    return HuntersGame(game_data)
+
+
 def main():
-    # Initialize Pygame
     pygame.init()
     screen_width, screen_height = 600, 600
     screen = pygame.display.set_mode((screen_width, screen_height))
     clock = pygame.time.Clock()
-
-    hunter_data: CreatureData = {
-        "pos_x": 10.0,
-        "pos_y": 10.0,
-        "speed": 1.0,
-        "detect_range": 500
-    }
-
-    prey_data: CreatureData = {
-        "pos_x": 100.0,
-        "pos_y": 150.0,
-        "speed": 3.0,
-        "detect_range": 100.0
-    }
-
-    h = Hunter(hunter_data)
-    p = Prey(prey_data)
-    creatures = [h, p]
+    
+    game_data = init_game_data()
 
     while True:
         for event in pygame.event.get():
@@ -121,14 +140,14 @@ def main():
 
         screen.fill((255, 255, 255))
 
-        for c in creatures:
-            c.step(creatures)
+        game_data.step()
 
-        for c in creatures:
+        for c in game_data.creatures:
             draw_creature(c, screen)
 
         pygame.display.flip()
         clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
